5c9641a06fe3e49e361d239039230d5f9b4e33f5
Tom Labiausse <tom01lab@gmail.com>
2024-03-16 11:27:30 +0100

    add argsparser

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ import os
 import sys
 import time
 import gymnasium as gym
+import argparse
 
 from agents import Agent, MCAgent, SARSALambdaAgent
-from trainers import Trainer, MCTrainer, SARSALambdaTrainer
+from trainers import MCTrainer, SARSALambdaTrainer
 
 
 def demo(agent_filename: str):
 
     # initiate environment
-    env = gym.make('TextFlappyBird-v0', height = 15, width = 20, pipe_gap = 12)
+    env = gym.make('TextFlappyBird-v0', height = 15, width = 20, pipe_gap = 4)
     # env = gym.make('TextFlappyBird-screen-v0', height = 15, width = 20, pipe_gap = 4)
 
     # initiate agent
-    agent = MCAgent.from_pretrained(agent_filename)
+    agent = Agent.from_pretrained(agent_filename)
 
     # episode
     obs, info = env.reset()
@@ -30,7 +31,7 @@ def demo(agent_filename: str):
         # Render the game
         os.system("clear")
         sys.stdout.write(env.render())
-        time.sleep(0.2) # FPS
+        time.sleep(0.1) # FPS
 
         # If player is dead break
         if done:
@@ -43,43 +44,78 @@ def main():
 
     env = gym.make('TextFlappyBird-v0', height = 15, width = 20, pipe_gap = 4)
 
-    EPISODES = 5000
+    EPISODES = 2000
     DISCOUNT_FACTOR = 1.0
-    EPSILON = 0.1
+    FINAL_EPSILON = 0.01
     LEARNING_RATE = 0.1
     TRACE_DECAY = 0.9
 
     EXPERIMENT_NAME = None
 
-    # MC
-    trainer = MCTrainer(
-        n_episodes=EPISODES,
-        discount_factor=DISCOUNT_FACTOR,
-        epsilon=EPSILON,
-        n_eval=100,
-        max_episode_length_eval=1000,
+    parser = argparse.ArgumentParser(description="Train an agent to play Flappy Bird.")
+    parser.add_argument(
+        "--mc",
+        action="store_true",
+        help="use this flag to train a Monte Carlo agent",
+    )
+    parser.add_argument(
+        "--sarsa-lambda",
+        action="store_true",
+        help="use this flag to train a SARSA-lambda agent",
+    )
+    parser.add_argument(
+        "--demo",
+        action="store_true",
+        help="use this flag to launch a demo of a trained agent",
+    )
+    parser.add_argument(
+        "--experiment-name",
+        type=str,
+        default=EXPERIMENT_NAME,
+        help="name of the experiment",
     )
 
-    # SARSALambda
-    # trainer = SARSALambdaTrainer(
-    #     n_episodes=EPISODES,
-    #     learnind_rate=LEARNING_RATE,
-    #     trace_decay=TRACE_DECAY,
-    #     discount_factor=DISCOUNT_FACTOR,
-    #     epsilon=EPSILON,
-    #     n_eval=100,
-    #     max_episode_length_eval=1000,
-    # )
+    args = parser.parse_args()
 
-    agent = trainer.train(
+    if args.mc and args.sarsa_lambda:
+        raise ValueError("Please specify only one agent type")
+    elif not args.mc and not args.sarsa_lambda:
+        raise ValueError("Please specify at least one agent type")
+    
+    if args.demo:
+        if args.mc:
+            demo("MCAgent")
+        elif args.sarsa_lambda:
+            demo("SARSALambdaAgent")
+        return
+    
+    if args.mc: # MC
+        trainer = MCTrainer(
+            n_episodes=EPISODES,
+            discount_factor=DISCOUNT_FACTOR,
+            final_epsilon=FINAL_EPSILON,
+            learning_rate=None,
+            n_eval=100,
+            max_episode_length_eval=1000,
+        )
+    elif args.sarsa_lambda: # SARSALambda
+        trainer = SARSALambdaTrainer(
+            n_episodes=EPISODES,
+            learnind_rate=LEARNING_RATE,
+            trace_decay=TRACE_DECAY,
+            discount_factor=DISCOUNT_FACTOR,
+            final_epsilon=FINAL_EPSILON,
+            n_eval=100,
+            max_episode_length_eval=1000,
+        )
+
+    _ = trainer.train(
         env=env,
-        experiment_name=EXPERIMENT_NAME,
+        experiment_name=args.experiment_name,
         save_plots=True,
         save_agent=True,
     )
     
 
 if __name__ == "__main__":
-    # main()
-    demo("MCAgent")
-    # demo("SARSALambdaAgent")
+    main()
